a18fd603fe54b29290b25fc854f47b8e99a67825
Christopher Vu <chris_nvu@yahoo.com>
2014-03-04 18:44:28 -0800

    pause menu2

---
+++ Assets/Pause_Menu.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause_Menu : MonoBehaviour {
+
+	public GUISkin pauseSkin;
+	private int SCREEN_WIDTH = Screen.width;
+	private int SCREEN_HEIGHT = Screen.height;
+	bool paused = false;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnGUI () {
+		GUI.skin = pauseSkin;
+
+		if (paused) {
+			if (GUI.Button (new Rect (0.20f * SCREEN_WIDTH, 0.2f * SCREEN_HEIGHT, 0.60f * SCREEN_WIDTH, 0.07f * SCREEN_HEIGHT), "Unpause")) {
+				paused = togglePause();
+			}
+
+			if (GUI.Button (new Rect (0.20f * SCREEN_WIDTH, 0.3f * SCREEN_HEIGHT, 0.60f * SCREEN_WIDTH, 0.07f * SCREEN_HEIGHT), "Main Menu")) {
+				paused = togglePause ();
+				Application.LoadLevel("Menu");
+			}
+		}
+	
+		else {
+			if (GUI.Button (new Rect (0.05f * SCREEN_WIDTH, 0.45f * SCREEN_HEIGHT, 0.2f * SCREEN_WIDTH, 0.05f * SCREEN_HEIGHT), "Pause")) {
+				paused = togglePause();
+			}
+		}
+
+	}
+
+	bool togglePause()
+	{
+		if (Time.timeScale == 0f) {
+			Time.timeScale = 1f;
+			return (false);
+		}
+		else {
+			Time.timeScale = 0f;
+			return(true);
+		}
+	}
+}
