a4729abe5efc6f7b7ab13fa9254aa4d9a89cadff
Emmanuel <Rodriguez>
2014-03-04 19:31:24 -0800

    Removed Product_Selection, updated Product_DB and Sample.csv. Sample.csv should now include all known categories for the known products

--- Assets/Product_DB.cs
+++ Assets/Product_DB.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Product_Stacks : MonoBehaviour {
+public class Product_DB : MonoBehaviour {
 
 	// Use this for initialization
 	// Need to create product stacks at the beginning of the game
@@ -13,24 +13,23 @@ public class Product_Stacks : MonoBehaviour {
 	private Item[] unknown;
 	private int max_index_known, max_index_unknown; // Not sure if needed, just in case
 
-	Product_Stacks(){
+	Product_DB(){
 		known = new Item[300];	// May need to adjust sizes
 		unknown = new Item[200];
 	}
 
 	void Start () {
 		// Parse product by product from CSV, placing info into Product objects
+		CsvFileReader reader = new CsvFileReader("Sample.csv");	// Use reader object to read in csv file
+		CsvRow row = new CsvRow();	// row will take in each row in csv file
 
-		// Create a "stack" of product objects
+		// run while there are still rows in the csv file
+		while(reader.ReadRow (row)){
+
+		}
+
 	}
-	
-/*	void startStackKnown(int Lower, Upper){
-		Item product;
-		
-		while( Lower <= Upper && (product = Item.Load(Lower)) != 0)
-			i++;
-			
-	}*/
+
 	
 	// Update is called once per frame
 	void Update () {
