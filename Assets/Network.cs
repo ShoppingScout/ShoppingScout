d85d27e94d72a60c73ca7107a1b80a0300255bec
Emmanuel <Rodriguez>
2014-05-27 01:44:47 -0700

    Added UdpKit library to project

---
+++ Assets/Network.cs
@@ -0,0 +1,54 @@
+﻿/*       Client Program      */
+
+using UnityEngine;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+//using System.Net.Sockets;
+using UdpKit;
+
+
+public class Network : MonoBehaviour{
+	UdpSocket socket;
+
+	/*
+	Network() {
+		
+		try {
+			TcpClient tcpclnt = new TcpClient();
+			Debug.Log("Connecting.....");
+			
+			tcpclnt.Connect("169.237.5.62",50000);
+			// use the ipaddress as in the server program
+			
+			Debug.Log("Connected");
+			Console.Write("Enter the string to be transmitted : ");
+			
+			String str=Console.ReadLine();
+			Stream stm = tcpclnt.GetStream();
+			
+			ASCIIEncoding asen= new ASCIIEncoding();
+			byte[] ba=asen.GetBytes(str);
+			Console.WriteLine("Transmitting.....");
+			
+			stm.Write(ba,0,ba.Length);
+			
+			byte[] bb=new byte[100];
+			int k=stm.Read(bb,0,100);
+			
+			for (int i=0;i<k;i++)
+				Console.Write(Convert.ToChar(bb[i]));
+			
+			tcpclnt.Close();
+		}
+		
+		catch (Exception e) {
+			Console.WriteLine("Error..... " + e.StackTrace);
+		}
+	}
+
+*/
+	
+}
+
